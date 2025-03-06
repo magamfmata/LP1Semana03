efeec3bb907cb6093b2447fce99828a3b5f55dba
Magali <magamayrmata@gmail.com>
2025-03-06 16:52:38 +0000

    Solicitar string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           Console.Write("Type a string: ");
+            string a = Console.ReadLine();
+        }
+    }
+}
