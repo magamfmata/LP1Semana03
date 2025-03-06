675b5ea550d49a916d846256e18cae7b8e0edaa6
Magali <magamayrmata@gmail.com>
2025-03-06 16:46:47 +0000

    Added RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {private static void Main(string[] args)
+        {
+            foreach (string superior in args)
+            {
+                if (superior.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                
+
+                if (superior.Length > 3)
+
+                {
+                    Console.WriteLine(superior);
+                }
+            }
+        }
+    }
+}
