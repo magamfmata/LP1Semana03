5152d943c5ec837a42853aecbefd039b65638cf9
Magali <magamayrmata@gmail.com>
2025-03-06 21:27:22 +0000

    Array Final

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ArrayMul
 {
@@ -6,7 +7,30 @@ namespace ArrayMul
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+          float[,] matrixA = new float[2, 2];
+
+          matrixA[0,0] = float.Parse(args[0], CultureInfo.InvariantCulture);
+          matrixA[0,1] = float.Parse(args[1], CultureInfo.InvariantCulture);
+          matrixA[1,0] = float.Parse(args[2], CultureInfo.InvariantCulture);
+          matrixA[1,1] = float.Parse(args[3], CultureInfo.InvariantCulture);
+
+          float[] vectorB = new float[2];
+          vectorB[0] = float.Parse(args[4], CultureInfo.InvariantCulture);
+          vectorB[1] = float.Parse(args[5], CultureInfo.InvariantCulture);
+
+          float[] total = new float[2];
+
+          for (int i = 0; i < 2; i++)
+        {
+            total[i] = 0;
+
+            for (int j = 0; j < 2; j++)
+            {
+                total[i] += matrixA[i, j] * vectorB[j];
+            }
         }
+          for (int i = 0; i < 2; i++);
+        }
+
     }
 }
